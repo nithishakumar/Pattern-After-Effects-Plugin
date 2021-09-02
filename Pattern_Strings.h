6b25aa95bc66dc0291d42e3c729dfd0ee25feba7
Nithisha Nantha Kumar <73742037+nithishakumar@users.noreply.github.com>
2021-09-02 07:28:43 -0400

    Plugin String Declarations

---
+++ Pattern_Strings.h
@@ -0,0 +1,45 @@
+/*******************************************************************/
+/*                                                                 */
+/*                      ADOBE CONFIDENTIAL                         */
+/*                   _ _ _ _ _ _ _ _ _ _ _ _ _                     */
+/*                                                                 */
+/* Copyright 2007 Adobe Systems Incorporated                       */
+/* All Rights Reserved.                                            */
+/*                                                                 */
+/* NOTICE:  All information contained herein is, and remains the   */
+/* property of Adobe Systems Incorporated and its suppliers, if    */
+/* any.  The intellectual and technical concepts contained         */
+/* herein are proprietary to Adobe Systems Incorporated and its    */
+/* suppliers and may be covered by U.S. and Foreign Patents,       */
+/* patents in process, and are protected by trade secret or        */
+/* copyright law.  Dissemination of this information or            */
+/* reproduction of this material is strictly forbidden unless      */
+/* prior written permission is obtained from Adobe Systems         */
+/* Incorporated.                                                   */
+/*                                                                 */
+/*******************************************************************/
+
+#pragma once
+
+typedef enum {
+	StrID_NONE, 
+	StrID_Name,
+	StrID_Description,
+	StrID_BgColor_Param_Name,
+	StrID_ChooseMode_Param_Name,
+	StrID_ModeChoices_Param_Name,
+	StrID_ChooseTiling_Param_Name,
+	StrID_TilingChoices_Param_Name,
+	StrID_SideLength_Param_Name, 
+	StrID_NumberofSides_Param_Name, 
+	StrID_Angle_Param_Name,
+	StrID_ChooseDelta_Param_Name,
+	StrID_DeltaChoices_Param_Name,
+	StrID_Delta_Param_Name,
+	StrID_HideTiling_Param_Name,
+	StrID_TColorChkBox_Param_Name,
+	StrID_TBgColor_Param_Name,
+	StrID_LineColor_Param_Name,
+	StrID_LineWidth_Param_Name,
+	StrID_NUMTYPES
+} StrIDType;
